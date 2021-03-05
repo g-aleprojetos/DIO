0b905daa82009dca8958a646412ced2de1208c08
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-05 16:25:41 -0300

    finalização da atividade Praticando programação em Kotlin

---
+++ Bootcamps/everis Kotlin developer/Praticando programação em Kotlin/src/main/kotlin/BobConduite.kt
@@ -0,0 +1,24 @@
+
+
+
+fun main() {
+
+    val inteiros = mutableListOf<Int>()
+    var soma:Int
+
+    for (i in 1..readLine()!!.toInt()) {
+
+        val list = readLine()!!.split(" ").map { it.toInt() }
+
+        if (list.size == 2){
+
+            soma = list[0] + list[1]
+            inteiros.add(soma)
+        }
+    }
+    for (i in inteiros){
+        println(i)
+
+    }
+
+}
---
+++ Bootcamps/everis Kotlin developer/Praticando programação em Kotlin/src/main/kotlin/ConversaoSimpleBase.kt
@@ -0,0 +1,41 @@
+
+
+
+fun main(args: Array<String>) {
+
+    val patternHexadecimal = """0[a-zA-Z].*""".toRegex()
+    val patternHex = """[a-zA-Z].*""".toRegex()
+    val listatotal = mutableListOf<String>()
+    var valor = true
+    while(valor) {
+
+        val leitura = readLine()!!.toString()
+
+        if (!leitura.contains(patternHexadecimal) && leitura.toInt() > 0 && leitura.toInt() < Integer.MAX_VALUE)  {
+
+            val n = leitura.toInt()
+
+            val decimal = Integer.toHexString(n).toUpperCase()
+            val decimalCompleto = "0x$decimal"
+            listatotal.add(decimalCompleto)
+
+        }
+        if(leitura == "-1"){
+
+            valor = false
+        }
+
+        if (leitura.contains(patternHexadecimal) && leitura.contains(patternHex)) {
+
+            val remover = leitura.removeRange(0,2)
+            val result: Int = remover.toInt(16)
+            listatotal.add(result.toString())
+        }
+    }
+    for (i in listatotal){
+
+        println(i)
+
+    }
+
+}
