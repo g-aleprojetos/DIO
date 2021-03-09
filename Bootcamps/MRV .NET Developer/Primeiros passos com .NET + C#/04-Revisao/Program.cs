800fd710206a5291535bb7739c645806ed794443
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-09 15:05:13 -0300

    aula 2 - Praticando primeiros passos

--- Bootcamps/MRV .NET Developer/Primeiros passos com .NET + C#/04-Revisao/Program.cs
+++ Bootcamps/MRV .NET Developer/Primeiros passos com .NET + C#/04-Revisao/Program.cs
@@ -1,10 +1,64 @@
 ﻿using System;
+using Revisao;
 
 namespace _04_Revisao
 {
     class Program
     {
         static void Main(string[] args)
+        {
+           Aluno[] alunos = new Aluno[5];
+           var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        System.Console.Write("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                       
+                        System.Console.Write("Informe a nota do aluno: ");
+                        
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                           throw new ArgumentException("Valor da nota deve ser decimal"); 
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                 System.Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        //todo: calcular media geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
         {
             System.Console.WriteLine();
             System.Console.WriteLine("Informe a opção desejada\n");
@@ -15,38 +69,7 @@ namespace _04_Revisao
             System.Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while(opcaoUsuario.ToUpper() != "x")
-            {
-               switch(opcaoUsuario)
-               {
-                    case "1":
-                        //todo: adicionar aluno
-                        break;
-
-                    case "2":
-                        //todo: listar alunos
-                        break;
-
-                    case "3":
-                        //todo: calcular media geral
-                        break;
-
-                    default:
-                    throw new ArgumentOutOfRangeException();
-               } 
-
-                          System.Console.WriteLine();
-            System.Console.WriteLine("Informe a opção desejada\n");
-            System.Console.WriteLine("1- Inserir novo aluno");
-            System.Console.WriteLine("2- Listar alunos");
-            System.Console.WriteLine("3- Calcular média geral");
-            System.Console.WriteLine("x- Sair");
-            System.Console.WriteLine();
-
-            opcaoUsuario = Console.ReadLine();
-            
-            }
+            return opcaoUsuario;
         }
     }
 }
