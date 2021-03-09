c4a1e1e49e6106a2a52325164c9901bc4d5ff408
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-09 12:24:19 -0300

    aula 1 - revisão introdutória

--- Bootcamps/MRV .NET Developer/Primeiros passos com .NET + C#/04-Revisao/Program.cs
+++ Bootcamps/MRV .NET Developer/Primeiros passos com .NET + C#/04-Revisao/Program.cs
@@ -6,7 +6,47 @@ namespace _04_Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Informe a opção desejada\n");
+            System.Console.WriteLine("1- Inserir novo aluno");
+            System.Console.WriteLine("2- Listar alunos");
+            System.Console.WriteLine("3- Calcular média geral");
+            System.Console.WriteLine("x- Sair");
+            System.Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while(opcaoUsuario.ToUpper() != "x")
+            {
+               switch(opcaoUsuario)
+               {
+                    case "1":
+                        //todo: adicionar aluno
+                        break;
+
+                    case "2":
+                        //todo: listar alunos
+                        break;
+
+                    case "3":
+                        //todo: calcular media geral
+                        break;
+
+                    default:
+                    throw new ArgumentOutOfRangeException();
+               } 
+
+                          System.Console.WriteLine();
+            System.Console.WriteLine("Informe a opção desejada\n");
+            System.Console.WriteLine("1- Inserir novo aluno");
+            System.Console.WriteLine("2- Listar alunos");
+            System.Console.WriteLine("3- Calcular média geral");
+            System.Console.WriteLine("x- Sair");
+            System.Console.WriteLine();
+
+            opcaoUsuario = Console.ReadLine();
+            
+            }
         }
     }
 }
