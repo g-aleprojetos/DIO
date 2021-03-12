2e1c71754ff73942a607d2340aa8bf61fa73b257
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-12 19:13:46 -0300

    entrega desafio 1 e 2

---
+++ Bootcamps/MRV .NET Developer/Solução de problemas básicos em C#/desafio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string[] entrada = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(entrada[0]);
+            int b = Convert.ToInt32(entrada[1]);
+
+
+            x = (a + b) ; //complete o código de acordo com o cálculo esperado
+
+            Console.WriteLine("X = {0}",Convert.ToInt32(x));
+
+        }
+    }
+}
---
+++ Bootcamps/MRV .NET Developer/Solução de problemas básicos em C#/desafio02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if( arr.Length >= 1 && arr.Length <=140 ) {//complete a condicional
+                Console.WriteLine("TWEET");
+            }
+            else{
+                Console.WriteLine("MUTE");
+            }
+        }
+    }
+}
