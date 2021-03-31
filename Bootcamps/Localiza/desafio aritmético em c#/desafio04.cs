a3b7d072df7a6c63efcb5bbb78bf443eef48b118
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-31 19:52:09 -0300

    Finalizando  desafio aritmético em c#

--- Bootcamps/Localiza/desafio aritmético em c#/desafio04.cs
+++ Bootcamps/Localiza/desafio aritmético em c#/desafio04.cs
@@ -4,33 +4,44 @@ class minhaClasse {
 
     static void Main(string[] args) { 
 
-            int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
-            int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTerminio;
-            //continue escrevendo a solucao
+      int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
+      int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTerminio;
+      string descricaoDiaInicio, descricaoDiaTermino;
 
-            string[] dadosInicio = Console.ReadLine().Split();
-            diaInicio = Convert.ToInt32(dadosInicio[1]);
+      string[] descricaoInicialEvento = Console.ReadLine().Split(' ');
+      descricaoDiaInicio = descricaoInicialEvento[0];
+      diaInicio = Convert.ToInt32(descricaoInicialEvento[1]);
 
-            string[] dadosMomentoInicio = Console.ReadLine().Split();
-            horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
-    
+      string[] dadosMomentoInicio = Console.ReadLine().Replace(" : ", " ").Split(' ');
+      horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
+      minutoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[1]);
+      segundoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[2]);
 
-            string[] dadosTermino = Console.ReadLine().Split();
-            diaTermino = Convert.ToInt32(dadosTermino[1]);
+      string[] descricaoTerminoEvento = Console.ReadLine().Split(' ');
+      descricaoDiaTermino = descricaoTerminoEvento[0];
+      diaTermino = Convert.ToInt32(descricaoTerminoEvento[1]);
 
-            string[] dadosMomentoTermino = Console.ReadLine().Split();
-            horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
-           
+      string[] dadosMomentoTermino = Console.ReadLine().Replace(" : ", " ").Split(' ');
+      horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
+      minutoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[1]);
+      segundoMomentoTerminio = Convert.ToInt32(dadosMomentoTermino[2]);
 
-            int transformaSegundosInicio = ();
-            int transformaSegundosTermino = ();
+      int segundoEventoInicio = diaInicio * 86400 + horaMomentoInicio * 3600 + minutoMomentoInicio * 60 + segundoMomentoInicio;
+      int segundoEventoTermino = diaTermino * 86400 + horaMomentoTermino * 3600 + minutoMomentoTermino * 60 + segundoMomentoTerminio;
+      var time = segundoEventoTermino - segundoEventoInicio;
 
-            int somaTotalSegundos = (            );
-            int W =  / ();
-          
+      int resto, resultado;
+      int dia = Math.DivRem(time, 86400, out resto);
+      resultado = resto;
+      int hora = Math.DivRem(resultado, 3600, out resto);
+      resultado = resto;
+      int minuto = Math.DivRem(resultado, 60, out resto);
+      int segundo = resto;
 
-            Console.WriteLine("{0} dia(s)", W);
-
+      Console.WriteLine("{0} dia(s)", dia);
+      Console.WriteLine("{0} hora(s)", hora);
+      Console.WriteLine("{0} minuto(s)", minuto);
+      Console.WriteLine("{0} segundo(s)", segundo);
 
     }
 
