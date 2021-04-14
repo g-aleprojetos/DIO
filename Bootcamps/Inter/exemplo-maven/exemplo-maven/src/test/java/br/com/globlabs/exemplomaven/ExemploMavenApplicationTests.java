d005893dbda202bb81c412f8176f439cf1f9f9c5
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-14 15:54:20 -0300

    exercicio java

---
+++ Bootcamps/Inter/exemplo-maven/exemplo-maven/src/test/java/br/com/globlabs/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globlabs.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
