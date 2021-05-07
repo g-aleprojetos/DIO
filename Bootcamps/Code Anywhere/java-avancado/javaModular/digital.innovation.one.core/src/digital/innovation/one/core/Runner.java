f3ce84db4c993f4bdb1227ad6c6654c1770c5ab2
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-07 20:47:29 -0300

    aula de java avançado

---
+++ Bootcamps/Code Anywhere/java-avancado/javaModular/digital.innovation.one.core/src/digital/innovation/one/core/Runner.java
@@ -0,0 +1,12 @@
+package digital.innovation.one.core;
+import digital.innovation.one.utils.operacao.Calculadora;
+import java.sql.Date;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println(calculadora.div(9,3));
+    }
+}
---
+++ Bootcamps/Code Anywhere/java-avancado/javaModular/digital.innovation.one.test/src/digital/innovation/one/test/Main.java
@@ -0,0 +1,9 @@
+package digital.innovation.one.test;
+
+import digital.innovation.one.utils.operacao.Calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+    }
+}
