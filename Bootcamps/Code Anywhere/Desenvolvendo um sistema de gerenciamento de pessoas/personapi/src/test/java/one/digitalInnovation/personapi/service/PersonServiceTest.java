701c8c6b45913241c8c91ca818c3ce4a679f6633
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-19 20:17:40 -0300

    Developed unit tests for create person

---
+++ Bootcamps/Code Anywhere/Desenvolvendo um sistema de gerenciamento de pessoas/personapi/src/test/java/one/digitalInnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,48 @@
+package one.digitalInnovation.personapi.service;
+
+import one.digitalInnovation.personapi.dto.request.PersonDTO;
+import one.digitalInnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalInnovation.personapi.entity.Person;
+import one.digitalInnovation.personapi.repository.PersonRepository;
+import one.digitalInnovation.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digitalInnovation.personapi.utils.PersonUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
