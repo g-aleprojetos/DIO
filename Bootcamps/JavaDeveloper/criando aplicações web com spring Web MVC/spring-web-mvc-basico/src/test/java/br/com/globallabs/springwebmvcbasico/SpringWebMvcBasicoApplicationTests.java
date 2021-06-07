f91abff79f12713bf86650422b467e001012cdff
g-aleprojetos <g.aleprojetos@gmail.com>
2021-06-07 10:17:18 -0300

    created project spring web mvc basico

---
+++ Bootcamps/JavaDeveloper/criando aplicações web com spring Web MVC/spring-web-mvc-basico/src/test/java/br/com/globallabs/springwebmvcbasico/SpringWebMvcBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.springwebmvcbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
