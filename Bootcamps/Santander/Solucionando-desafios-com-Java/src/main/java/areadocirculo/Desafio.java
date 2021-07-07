5de68da1fbcf53b8949c0af3bb5a1cae2816a517
g-aleprojetos <g.aleprojetos@gmail.com>
2021-07-06 22:08:07 -0300

    Desafio 1 finalizado

---
+++ Bootcamps/Santander/Solucionando-desafios-com-Java/src/main/java/areadocirculo/Desafio.java
@@ -0,0 +1,17 @@
+package areadocirculo;
+
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args){
+        double area ;
+        double raio;
+
+        Scanner scan = new Scanner(System.in);
+        raio = Double.parseDouble(scan.nextLine());
+
+        area = 3.14159 * (Math.pow( raio ,2));
+
+        System.out.printf("A=%.4f\n", area);
+    }
+}
---
+++ Bootcamps/Santander/Solucionando-desafios-com-Java/src/main/java/media3/Desafio.java
@@ -0,0 +1,49 @@
+package media3;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        Locale.setDefault(new Locale("en", "US"));
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        float n1,n2,n3,n4,media,emexame,emexamefinal;
+
+        n1 = sc.nextFloat();
+        n2 = sc.nextFloat();
+        n3 = sc.nextFloat();
+        n4 = sc.nextFloat();
+
+        media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
+
+        System.out.printf("Media: %.1f\n",media);
+
+        if ( media >= 7.0 ){
+            System.out.printf("Aluno aprovado.\n");
+        }
+        else if (media < 5.0 ){
+            System.out.printf("Aluno reprovado.\n");
+        }
+        else {
+            System.out.printf("Aluno em exame.\n");
+            emexame = sc.nextFloat();
+            System.out.printf("Nota do exame: %.1f\n",emexame );
+
+            emexamefinal = (emexame + media)/2;
+
+            if( emexamefinal >= 5.0 ){
+                System.out.printf("Aluno aprovado.\n");
+                System.out.printf("Media final: %.1f\n", emexamefinal );
+            }
+            else{
+                System.out.printf("Aluno reprovado.\n");
+                System.out.printf("Media final: %.1f\n", emexamefinal );
+            }
+        }
+        sc.close();
+    }
+}
